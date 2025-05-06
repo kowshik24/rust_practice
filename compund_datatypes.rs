00802edf6308f3e11fe8eadfa0df18eab5d31128
kowshik24 <kowshikcseruet1998@gmail.com>
2025-05-06 14:16:13 +0600

    Updated tuples, string slices

--- compund_datatypes.rs
+++ compund_datatypes.rs
@@ -23,10 +23,43 @@ fn main(){
 
     let human: (String, i32, bool) = ("Alice".to_string(), 30, false);
     println!("Human Tuple: {:?}", human);
+    
+    let my_mix_tuple = ("Kratos", 23, true, [1,2,3,4,5]);
+    println!("My Mix Tuple: {:?}", my_mix_tuple);
+
+    // Slices: [1,2,3,4,5]
+
+    let number_slices:&[i32] = &[1,2,3,4,5];
+    println!("Number Slices: {:?}", number_slices);
+
+    let animal_slices:&[&str] = &["Lion", "Elephant", "Crocodile"];
+    println!("Animal Slices: {:?}", animal_slices);
+
+    let book_slices:&[&String] = &[ &"IT".to_string(), &"Harry Potter".to_string(), &"ZEN".to_string()];
+    println!("Book Slices: {:?}", book_slices);
 
 
+    // Strings vs String Slices(&str)
+    // Strings [growable, mutable, owned string type]
+
+    let mut stone_cold: String = String::from("Hell, ");
+    
+
+    stone_cold.push_str("Yeah!");
+
+    println!("Storne cold says: {:?}", stone_cold);
+
+    // B - &str (String Slice)
+
+    let string: String = String::from("Hello, World!");
+    let slice: &str = &string[0..5];
+    println!("Slice value: {}",slice);
 
 
     
 
 }
+
+// fn print(){
+//     println!("SLICE: {}",slice);
+// }
